114a8fcf5267b686d734c7f22fb943045e284df6
Vishal Kumar <vishal700401@hmail.com>
2023-08-14 19:38:27 +0530

    new changes added

--- src/app/api/admin/cases/invitations/accept/route.js
+++ src/app/api/admin/cases/invitations/accept/route.js
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { sendInvitationSchema } from "@/joi/casesSchema";
 import common from "@/utils/common";
-import sendMail from "@/utils/sendMail";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 
@@ -13,9 +11,8 @@ export async function POST(request) {
   let response = {};
   try {
     const data = await request.json();
-
     let caseModel = null;
-    if (parseInt(data?.id)) {
+    if (data?.id) {
       caseModel = await prisma.cases.findUnique({
         where: {
           id: parseInt(data?.id),
@@ -24,7 +21,7 @@ export async function POST(request) {
           case_invitations: {
             where: {
               status: {
-                equals: 1, // Accepted
+                equals: 1, // it means already accepted
               },
             },
           },
@@ -32,67 +29,42 @@ export async function POST(request) {
       });
     }
 
-    if (caseModel) {
-      if (usersModel) {
-        if (!isAccepted) {
-          await prisma.$transaction(async (tx) => {
-            let activeInvitations = await tx.case_invitations.findMany({
-              where: {
-                case_id: data.case_id,
-                user_id: {
-                  in: data.users,
-                },
-                status: 0,
-              },
-            });
+    if (caseModel && caseModel?.case_invitations.length <= 0) {
+      await prisma.$transaction(async (tx) => {
+        console.log("here-----");
+        // First update all with expired.
+        await tx.case_invitations.update({
+          data: {
+            status: 2,
+          },
+          where: {
+            case_id: {
+              equals: caseModel.id,
+            },
+          },
+        });
 
-            let createInvUsers = usersModel;
-            if (activeInvitations && activeInvitations.length > 0) {
-              let activeUsers = activeInvitations.map((item) => item.id);
-              createInvUsers = createInvUsers.filter(
-                (item) => !activeUsers.includes(item.id)
-              );
-            }
+        // First update one with accepted.
+        await tx.case_invitations.update({
+          data: {
+            status: 1,
+          },
+          where: {
+            case_id: caseModel.id,
+            user_id: caseModel.user_id,
+          },
+        });
 
-            // Entry invited users
-            await tx.case_invitations.createMany({
-              data: createInvUsers.map((user) => {
-                return {
-                  case_id: data.case_id,
-                  user_id: user.id,
-                };
-              }),
-            });
-            // Send mail to invite eco providers
-            await usersModel.forEach(async (user) => {
-              await sendMail({
-                to: process.env.TEST_USER_EMAIL || user.email,
-                templateId: common.params.templateId.sendCaseInvitation,
-                modelsData: {
-                  users: user,
-                  cases: caseModel,
-                },
-              });
-            });
-            response.success = true;
-            response.message = "Invitation has been sent successfully.";
-          });
-        } else {
-          response.error = true;
-          response.message =
-            "This case has already been assigned an echo provider.";
-        }
-      } else {
-        response.error = true;
-        response.message = "The selected users are not valid.";
-      }
+        response.success = true;
+        response.message = "Invitation has been accepted successfully.";
+      });
     } else {
       response.error = true;
       response.message = "Selected case has been expired.";
     }
   } catch (error) {
     response.error = true;
-    response.message = common.getErrors(error);
+    response.message = error.message;
   }
   return NextResponse.json(response);
 }
