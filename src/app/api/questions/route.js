b756ebdd7d4c61aa4da555fcd8755c83b553538e
Vishal Kumar <vishal700401@hmail.com>
2023-08-24 19:37:43 +0530

    dummy changes

--- src/app/api/questions/route.js
+++ src/app/api/questions/route.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-
+// ----
 const prisma = new PrismaClient();
 export async function GET(request) {
   return NextResponse.json({
