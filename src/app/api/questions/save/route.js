c8d5dde76655ca9535d487be0690534bfa8b7adc
Vishal Kumar <vishal700401@hmail.com>
2023-08-26 16:48:37 +0530

    added new fileds in users table

--- src/app/api/questions/save/route.js
+++ src/app/api/questions/save/route.js
@@ -1,40 +1,41 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import Joi from "joi";
 import common from "@/utils/common";
+import Joi from "joi";
+
+const prisma = new PrismaClient();
 
 export async function POST(request) {
-  let response = {};
-  const prisma = new PrismaClient();
-  try {
-    let data = await request.json();
+  let response = {};  
+  try {    
+    const data = await request.json();
+
     const schema = Joi.object({
-      question: Joi.string().max(400).required(),
-      options: Joi.array()
-        .items(Joi.string().max(400).label("Option"))
-        .min(2)
-        .max(10)
-        .required(),
-      answer: Joi.number().min(1).required().messages({
-        "number.base": "Select a correct answer.",
-      }),
+      registration: Joi.array().required(),
+      answers: Joi.array().required(),
     });
-    data = await schema.validateAsync(data, { abortEarly: false });
-    // Create a new question with options
-    const addQuestion = await prisma.questions.create({
-      data: {
-        question: data.question,
-        options: {
-          create: data.options.map((opt, index) => {
-            return { option: opt, status: index+1 === data.answer };
-          }),
-        },
-      },
-    });
-    if (addQuestion) {
-      response.success = true;
-      response.message = "New question added successfully.";
-    }
+    const fields = await schema.validateAsync(data, { abortEarly: true });
+
+    prisma.$transaction(async (tx)=>{
+      await tx.attorney_answers.create({
+        
+      })
+    })
+
+    // const registration = await prisma.questions.create({
+    //   data: {
+    //     question: data.question,
+    //     options: {
+    //       create: data.options.map((opt, index) => {
+    //         return { option: opt, status: index + 1 === data.answer };
+    //       }),
+    //     },
+    //   },
+    // });
+    // if (addQuestion) {
+    //   response.success = true;
+    //   response.message = "New question added successfully.";
+    // }
   } catch (error) {
     response.error = true;
     response.message = await common.getErrors(error);
