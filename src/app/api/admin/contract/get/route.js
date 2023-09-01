4f9b39f5e3f85688c029514506b4a92906b4d891
Vikas Kumar <pksbbr.jha@gmail.com>
2023-09-01 16:49:55 +0530

    get contract fixed

--- src/app/api/admin/contract/get/route.js
+++ src/app/api/admin/contract/get/route.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
+import { revalidatePath } from 'next/cache'
 
-export async function GET() {
+export async function GET(request) {
+  const path = request.nextUrl.searchParams.get('path');
+  revalidatePath(path)
   let response = {};
   try {
     const contract = await prisma.contracts.findUnique({
