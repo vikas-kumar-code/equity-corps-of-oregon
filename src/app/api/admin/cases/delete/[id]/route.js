a868432b3cac99437613297ac817777d2856ae9c
mitizdev <mitizdev@gmail.com>
2023-09-05 17:04:51 +0530

    changes done

--- src/app/api/admin/cases/delete/[id]/route.js
+++ src/app/api/admin/cases/delete/[id]/route.js
@@ -1,34 +1,41 @@
 import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export async function DELETE(request, data) {
-
   let response = {};
   let deletedId = Number(data.params.id);
   try {
-    const doc = await prisma.case_documents.findUnique({
-      where: {
-        id: deletedId
-      }
-    });
-    if (doc) {
-      // Delete the document associated with the cases
-      const filePath = path.join(process.cwd(), 'public','uploads','case_documents', doc.file_name);
-        if (fs.existsSync(filePath)) {
-          fs.unlinkSync(filePath);
-        }
-      }
-      
     if (deletedId) {
+      const docs = await prisma.cases.findUnique({
+        where: {
+          id: deletedId,
+        },
+        include: {
+          case_documents: true,
+        },
+      });
+      
       const deleted = await prisma.cases.delete({
         where: {
           id: deletedId,
         },
       });
-      if (deleted) {
 
+      if (deleted) {
+        docs.case_documents.forEach((doc) => {
+          const filePath = path.join(
+            process.cwd(),
+            "public",
+            "uploads",
+            "case_documents",
+            doc.file_name
+          );
+          if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+          }
+        });
         response.success = true;
         response.message = "Case has been deleted successfully.";
       }
@@ -84,5 +91,3 @@ export async function DELETE(request, data) {
 //   }
 //   return NextResponse.json(response);
 // }
-
-
