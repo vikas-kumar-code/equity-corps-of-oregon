fe7d6e8a94780b917e41a766390071899f6d1fa0
mitizdev <mitizdev@gmail.com>
2023-09-09 13:28:13 +0530

    src deleted

--- src/joi/questionsSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi')
-
-const questionSchema = Joi.object({
-    registration: Joi.object().required(),
-    attorney_answers: Joi.object().required(),
-  });
-
-export default questionSchema;
