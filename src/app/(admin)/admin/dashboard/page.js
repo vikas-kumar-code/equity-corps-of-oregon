05f91b3882e12171e2fef4f07748cb8efa24e59f
Vikas Kumar <pksbbr.jha@gmail.com>
2023-09-22 21:05:59 +0530

    pulling latest changes

--- src/app/(admin)/admin/dashboard/page.js
+++ src/app/(admin)/admin/dashboard/page.js
@@ -1,101 +1,12 @@
+import TotalCounts from "./components/TotalCounts";
+
 export const metadata = {
   title: `Dashboard - Admin Panel`
 }
 export default async function Dashboard() {
   return (
     <div className="content-wrapper">
-      <div className="row">
-        <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-9">
-                  <div className="d-flex align-items-center align-self-start">
-                    <h3 className="mb-0">$12.34</h3>
-                    <p className="text-success ms-2 mb-0 font-weight-medium">
-                      +3.5%
-                    </p>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <div className="icon icon-box-success ">
-                    <span className="mdi mdi-arrow-top-right icon-item"></span>
-                  </div>
-                </div>
-              </div>
-              <h6 className="text-muted font-weight-normal">
-                Potential growth
-              </h6>
-            </div>
-          </div>
-        </div>
-        <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-9">
-                  <div className="d-flex align-items-center align-self-start">
-                    <h3 className="mb-0">$17.34</h3>
-                    <p className="text-success ms-2 mb-0 font-weight-medium">
-                      +11%
-                    </p>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <div className="icon icon-box-success">
-                    <span className="mdi mdi-arrow-top-right icon-item"></span>
-                  </div>
-                </div>
-              </div>
-              <h6 className="text-muted font-weight-normal">Revenue current</h6>
-            </div>
-          </div>
-        </div>
-        <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-9">
-                  <div className="d-flex align-items-center align-self-start">
-                    <h3 className="mb-0">$12.34</h3>
-                    <p className="text-danger ms-2 mb-0 font-weight-medium">
-                      -2.4%
-                    </p>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <div className="icon icon-box-danger">
-                    <span className="mdi mdi-arrow-bottom-left icon-item"></span>
-                  </div>
-                </div>
-              </div>
-              <h6 className="text-muted font-weight-normal">Daily Income</h6>
-            </div>
-          </div>
-        </div>
-        <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-9">
-                  <div className="d-flex align-items-center align-self-start">
-                    <h3 className="mb-0">$31.53</h3>
-                    <p className="text-success ms-2 mb-0 font-weight-medium">
-                      +3.5%
-                    </p>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <div className="icon icon-box-success ">
-                    <span className="mdi mdi-arrow-top-right icon-item"></span>
-                  </div>
-                </div>
-              </div>
-              <h6 className="text-muted font-weight-normal">Expense current</h6>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TotalCounts />
       <div className="row">
         <div className="col-md-4 grid-margin stretch-card">
           <div className="card">
--- src/app/api/admin/dashboard/route.js
+++ src/app/api/admin/dashboard/route.js
@@ -1,27 +1,41 @@
 import { NextResponse } from "next/server";
-import { headers } from 'next/headers'
-
-
 import prisma from "@/utils/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
 export async function GET(request) {
-    const headerList = headers();
+    const session = await getServerSession(authOptions);
+    let counts = [];
+    const totalUsers = await prisma.users.count();
 
 
-    const toalUsers = await prisma.users.count();
-
-    const activeUsers = await prisma.users.count({
+    const totalCases = await prisma.cases.count({
         where: {
             status: 1
         }
     });
+    counts = [
+        { count: totalCases, label: 'Total Cases', icon: 'mdi mdi-alpha-c-circle' },
+        { count: totalUsers, label: 'Total Users', icon: 'mdi mdi-account' },
+    ];
+
+    if (session.user.role_id === 3) {
+        const totalInvitations = await prisma.case_invitations.count({
+            where: {
+                user_id: session.user.id,
+            }
+        });
+        counts = [
+            ...counts,
+            { count: totalInvitations, label: 'Total Case Invitations', icon: 'mdi mdi-alpha-c-circle' },
+        ]
+    }
+
 
     return NextResponse.json({
         success: true,
         records: {
-            todaysLoans: 5,
-            totalLoans: 5,
-            toalUsers: toalUsers,
-            activeUsers: activeUsers
+            counts: counts
         }
     });
 }
