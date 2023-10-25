259d36de1c17a51208184f113fe2734a708c90c5
Jose <jcruzdesign@gmail.com>
2023-10-25 10:38:39 -0700

    fix: mail template

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -14,21 +14,21 @@ const sendMail = async (
 ) => {
   try {
     // Setup Transporter
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
-      },
-    });
     // const transporter = nodemailer.createTransport({
-    //   host: process.env.MAIL_HOST,      
-    //   secure: true, // upgrade later with STARTTLS
+    //   service: "gmail",
     //   auth: {
     //     user: process.env.MAIL_USERNAME,
     //     pass: process.env.MAIL_PASSWORD,
     //   },
     // });
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,      
+      secure: true, // upgrade later with STARTTLS
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
     const mailData = await createMailData(
       options.templateId,
       options.modelsData
