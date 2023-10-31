4eaf978595823c98bf20220810134a55754290f6
mitizdev <mitizdev@gmail.com>
2023-10-31 19:18:21 +0530

    fixed sidebar issue

--- public/script.js
+++ public/script.js
@@ -1,27 +1,34 @@
-document.getElementById('toggle-menu').addEventListener('click', () => {
-    if (document.body.className.includes('sidebar-icon-only'))
-        document.body.className = 'whitetheme';
-    else
-        document.body.className = 'whitetheme sidebar-icon-only';
+document.getElementById("toggle-menu").addEventListener("click", () => {
+  if (document.body.className.includes("sidebar-icon-only"))
+    document.body.className = "whitetheme";
+  else document.body.className = "whitetheme sidebar-icon-only";
 });
 
-let sidebar = document.getElementById('offcanvasSidebar')
-document.getElementById('btn-z').addEventListener('click', () => { 
-    sidebar.classList.toggle('show');
-    document.body.classList.toggle('overflow-hidden');
-    document.body.classList.toggle('pe-0');
-    sidebar.classList.toggle('visible');
-    sidebar.classList.toggle('w-0');
+let sidebar = document.getElementById("offcanvasSidebar");
+let sidebarBackdrop = document.getElementById("sidebar-backdrop");
+document.getElementById("sidebar-toggle-btn").addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  sidebarBackdrop.classList.toggle("active");
 });
 
+document.getElementById("sidebar-backdrop").addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  sidebarBackdrop.classList.toggle("active");
+});
 
+document.querySelectorAll(".nav-item").forEach(item => {
+  item.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    sidebarBackdrop.classList.toggle("active");
+  })
+});
 
 window.onload = () => {
-    /* let navigationMenu = document.querySelectorAll(".admin-panel-nav-item");
+  /* let navigationMenu = document.querySelectorAll(".admin-panel-nav-item");
     console.log(navigationMenu)
     navigationMenu.forEach(menu => {
         menu.addEventListener('mouseover', (event) => {
             alert('vikas');
         });
     }) */
-}
+};
